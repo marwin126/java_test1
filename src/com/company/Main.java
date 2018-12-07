084368eb301b0d53ddf91fbb7c827b60c59980c9
marwin126 <xihezhiliu@126.com>
2018-12-07 18:00:10 +0800

    1

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int df=1;
+        System.out.println("hello world!");
+        System.out.println(df);
+        System.out.println();
+    }
+
+
+}
