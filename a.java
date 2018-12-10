10afaba78ad406ffdf3978749b071cf1aadb8f3c
marwin126 <xihezhiliu@126.com>
2018-12-10 11:06:30 +0800

    dev1 first

--- a.java
+++ a.java
@@ -1,7 +1,7 @@
 public class a {
     public static void main(String[] args) {
         System.out.println("曹美婷是最丑的小仙女");
-        System.out.println();
+        System.out.println("new dev1");
     }
 
 }
